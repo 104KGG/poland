aa26270fdcb5ada2634f9f19aeb677ca94efc859
104KGG <156039386+104KGG@users.noreply.github.com>
2024-08-26 08:48:23 +0800

    Delete 622d75c818468_v.js

--- polandsweethoney03/622d75c818468_v.js
+++
@@ -1,3 +0,0 @@
-var array_day = new Array("Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota");
-
-var array_month = new Array("Styczeń", "Luty", "Marsz", "Kwiecień", "Może", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień");
